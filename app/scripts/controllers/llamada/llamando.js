02dbe21909ea474d2aab4482fb1fc9737fba5334
vladwelt <vladwelt@gmail.com>
2015-01-01 01:21:32 -0400

    [EDIT] llamando plantilla inicial.

--- app/scripts/controllers/llamada/llamando.js
+++ app/scripts/controllers/llamada/llamando.js
@@ -8,12 +8,34 @@
  * Controller of the smartPointViewsApp
  */
 angular.module('smartPointViewsApp')
-  .controller('LlamadaLlamandoCtrl', function ($scope,$routeParams,$log) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('LlamadaLlamandoCtrl', function ($scope,$routeParams,$log,$interval) {
+    $scope.tiempo=60;
+    var alto;
     $scope.numero = $routeParams.numero;
+    $scope.contar = function(){
+        if ( angular.isDefined(alto) ) return;
+
+        alto = $interval(function(){
+            if($scope.tiempo>0) {
+                $scope.tiempo=$scope.tiempo -1;
+            } else {
+                $scope.altoContar();
+            }
+        },1000);
+    };
+
+    $scope.altoContar = function(){
+        if (angular.isDefined(alto)) {
+            $interval.cancel(alto);
+            alto = undefined;
+        }
+    };
+
+    $scope.$on('$destroy', function() {
+        $scope.altoContar();
+    });
+
     $log.debug($scope.numero);
-  });
+    $log.debug($scope.contador);
+    $scope.contar();
+});
