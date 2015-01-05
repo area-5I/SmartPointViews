4c315717a3452a5afca898f21097502536d90b0c
vladwelt <vladwelt@gmail.com>
2015-01-05 08:55:36 -0400

    [EDIT] notas's view finish and missing styles.

--- app/scripts/controllers/llamada/notas.js
+++ app/scripts/controllers/llamada/notas.js
@@ -9,24 +9,57 @@
  */
 angular.module('smartPointViewsApp')
   .controller('LlamadaNotasCtrl', function ($scope) {
-      $scope.teclar=function(t1, t2) {
-        if(typeof t2 !== "undefined") {
-            s += p2;
+
+      $scope.miTexto = "";
+      $scope.uppercase = false;
+      $scope.shiftActivado = false;
+
+      $scope.teclar = function(t1, t2) {
+        if($scope.shiftActivado) {
+            if(typeof t2 !== "undefined") {
+                $scope.miTexto += t2;
+            } else {
+                $scope.miTexto += t1.toUpperCase();
+            }
+            $scope.shift();
+        } else {
+            $scope.miTexto=$scope.miTexto+t1;
         }
       };
-      $scope.shiftActivado=true;
-      $scope.shift=function() {
-        
-      };
-      $scope.borrar=function(){
-      
-      };
-      $scope.space=function(){
-      
+
+      $scope.shift = function() {
+        $scope.shiftActivado = !$scope.shiftActivado;
+        if($scope.shiftActivado && $scope.uppercase){
+            $scope.casse = "";
+        } else if($scope.shiftActivado && !$scope.uppercase) {
+            $scope.casse = "uppercase";
+        }
+        else if(!$scope.shiftActivado && $scope.uppercase){
+            $scope.casse = "uppercase";
+        } else {
+            $scope.casse = "";
+        }
       };
 
-      $scope.teclarreturn=function(){
+      $scope.capslock = function() {
+        $scope.uppercase = !$scope.uppercase;
+        $scope.casse = $scope.uppercase ? "uppercase" : "";
+      };
+
+      $scope.borrar = function(){
+        $scope.miTexto=$scope.miTexto.substr(0, $scope.miTexto.length - 1) 
+      };
+
+      $scope.space = function(){
+        $scope.miTexto += " "; 
+      };
+
+      $scope.teclarreturn = function(){
+          $scope.miTexto += "\n";
+      };
+
+      $scope.tab=function(){
+        $scope.miTexto += "\t";
       };
 
   });
-
