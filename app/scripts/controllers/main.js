377829d1545386fad57420dab019111fd3e60f0b
vladwelt <vladwelt@gmail.com>
2015-01-07 01:06:28 -0400

    [EDIT] styles and view for sleeping app, when 5 seconds passed.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,13 @@
  * Controller of the smartPointViewsApp
  */
 angular.module('smartPointViewsApp')
-  .controller('MainCtrl', function ($scope,changeColorHeader,$log) {
+  .controller('MainCtrl', function ($scope,changeColorHeader,$log,$interval) {
+    $scope.reposo="init";
     changeColorHeader.setColor("inicioHeader");
     $scope.headerColor = function() {
         return changeColorHeader.getColor();
     };
+    $interval(function(){
+        $scope.reposo="initReposo";
+    },5000);
 });
