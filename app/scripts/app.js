eac587adfa60eaed51533964686463ec875a7c8f
vladwelt <vladwelt@gmail.com>
2015-01-18 22:56:43 -0400

    [EDIT] Added directive for modals.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -111,5 +111,26 @@ angular
         });
         updateLater();
     }
-});
 
+    })
+    .directive('modalDialog', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                show: '='
+            },
+            replace: true,
+            transclude: true,
+            link: function(scope, element, attrs) {
+                scope.dialogStyle = {};
+                if (attrs.width)
+                    scope.dialogStyle.width = attrs.width;    
+                if (attrs.height)
+                    scope.dialogStyle.height = attrs.height;
+                    scope.hideModal = function() {
+                        scope.show = false;
+                    };
+                },
+            template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></formatdiv><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</formatdiv><div class='ng-modal-dialog-content' ng-transclude></formatdiv></formatdiv></formatdiv>"
+            };
+    });
