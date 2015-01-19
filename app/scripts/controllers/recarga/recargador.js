b2b47ff3e322afe888e78dc1989bc069ad37a592
vladwelt <vladwelt@gmail.com>
2015-01-18 22:59:20 -0400

    [EDIT] added modal view for recargador and styles for modals in notas and recargador.

--- app/scripts/controllers/recarga/recargador.js
+++ app/scripts/controllers/recarga/recargador.js
@@ -11,4 +11,8 @@ angular.module('smartPointViewsApp')
   .controller('RecargaRecargadorCtrl', function ($scope,$stateParams) {
 
       $scope.empresaId = $stateParams.empresaId;
+      $scope.modalShown = false;
+      $scope.toggleModal = function() {
+          $scope.modalShown = !$scope.modalShown;
+      };
   });
