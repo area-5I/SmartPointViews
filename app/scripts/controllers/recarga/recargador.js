942e22323823c261cc7aa2ac3b272666fa0727d5
vladwelt <vladwelt@gmail.com>
2015-01-20 01:40:10 -0400

    [EDIT][ADD] button delete with orange color and controller for recargar.

--- app/scripts/controllers/recarga/recargador.js
+++ app/scripts/controllers/recarga/recargador.js
@@ -15,4 +15,12 @@ angular.module('smartPointViewsApp')
       $scope.toggleModal = function() {
           $scope.modalShown = !$scope.modalShown;
       };
+      $scope.moneda=0;
+      $scope.numero="";
+      $scope.marcar=function(teclaPulsada) {
+        $scope.numero=$scope.numero.concat(teclaPulsada);
+      }
+      $scope.borrar=function() {
+        $scope.numero=$scope.numero.slice(0,$scope.numero.length-1);
+      }
   });
