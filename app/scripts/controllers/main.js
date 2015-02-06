3a08fe0c0593ec1618a2f0ec0c6b3244de1cfb7e
vladwelt <vladwelt@gmail.com>
2015-02-05 23:21:04 -0400

    [EDT] Interaccion with signal test.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,7 @@
 angular.module('smartPointViewsApp')
   .controller('MainCtrl',['$scope','changeColorHeader','$log','$timeout',function ($scope,changeColorHeader,$log,$timeout) {
     $scope.reposo="init";
+    $scope.potency=[1,2,3,4];
     changeColorHeader.setColor("inicioHeader");
     $scope.headerColor = function() {
         return changeColorHeader.getColor();
@@ -25,4 +26,16 @@ angular.module('smartPointViewsApp')
         $scope.reposo="initReposo";
         $scope.changePosition="";
     },4950);
+    $timeout(function(){
+        $scope.potency.pop();
+    },2000);
+    $timeout(function(){
+        $scope.potency.pop();
+    },4000);
+    $timeout(function(){
+        $scope.potency.push(3);
+    },6000);
+    $timeout(function(){
+        $scope.potency.push(4);
+    },8000);
 }]);
